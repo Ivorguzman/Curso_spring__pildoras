d8bfc9fba6c879fbb67c4e91d74d11bd32aec65c
ivorguzman <ivorguzman7@gmail.com>
2024-01-06 15:01:06 -0400

    06012024

---
+++ ProyectoSpringUno/src/main/java/es/pildorasIoC/Singleton_Y_Prototype/UsoDemoSingeltonPrototype.java
@@ -0,0 +1,27 @@
+package es.pildorasIoC.Singleton_Y_Prototype;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UsoDemoSingeltonPrototype {
+
+	public static void main(String[] args) {
+
+
+
+		// 01 cargar xml de configuración
+		@SuppressWarnings("resource")
+		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContextPatrones.xml");
+
+
+		// O2 Creacion de bean(s) con metodo getBean("...") contenedor de Spring
+		SecretarioEmpleados Maria = contexto.getBean("miSecretarioEmpleados", SecretarioEmpleados.class);
+		SecretarioEmpleados Pepe = contexto.getBean("miSecretarioEmpleados", SecretarioEmpleados.class);
+
+
+		System.out.println("Maria ==> " + Maria.hashCode());
+		System.out.println("Pepe ==>  " + Pepe.hashCode());
+
+
+	}
+
+}
